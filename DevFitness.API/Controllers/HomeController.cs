3ef99a8bdfcd409823823e82dd6b81321fa6c56c
Matheus Henrique <matheushenri4141@gmail.com>
2023-02-28 14:15:15 -0300

    Ajuste para rodar no vs code

---
+++ DevFitness.API/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        [Route("/index")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
